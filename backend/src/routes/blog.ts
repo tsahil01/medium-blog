0c1dfefdfa8aa93621b33f0c9a7bcef30cfd7280
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-25 23:45:03 +0530

    jwt for auth

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,10 +1,26 @@
 import { Hono } from "hono";
+import { decode, verify } from 'hono/jwt'
 
 const blog = new Hono<{
     Bindings: {
-      DATABASE_URL: string
+      DATABASE_URL: string,
+      JWT_SECRET: string
     }
   }>();
+
+blog.use('/*', async (c, next) => {
+    const header = await c.req.header("authorization") || ""; // Bearer `token`
+    const token = header.split(" ")[1]; 
+    try{
+        const response = await verify(token, c.env.JWT_SECRET);
+        await next();
+    } catch(e){
+        c.status(403);
+        return c.json({
+            msg: "auth err"
+        })
+    }
+})
 
 blog.post('/', (c)=>{
     return c.json({
