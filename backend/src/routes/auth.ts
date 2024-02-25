37abcbc3fa2c1e1a96d1f456538af72419b3bb46
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-25 18:55:33 +0530

    Add @prisma/extension-accelerate dependency and update signup route

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,11 +1,23 @@
 import { Hono } from "hono";
+import prisma from "../prismaScript";
 
 const auth = new Hono();
 
-auth.post('/signup',(c)=>{
-    return c.json({
-        msg: "on /api/v1/singup"
-    })
+auth.post('/signup', async (c)=>{
+    const body = await c.req.json();
+    try {
+        const user = await prisma.user.create({
+            data: {
+                email: body.email,
+                password: body.password
+            }
+        });
+        return c.json({
+            msg: "Signup done"
+        })
+    } catch(e){
+        return c.status(403);
+    }
 })
 
 auth.post('/signin',(c)=>{
