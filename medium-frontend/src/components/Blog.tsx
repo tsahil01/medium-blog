b7aa90d7811e695875c48cb0c0fbe1f349854322
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-01 20:18:23 +0530

    updates in dev daries

--- medium-frontend/src/components/Blog.tsx
+++ medium-frontend/src/components/Blog.tsx
@@ -1,15 +1,32 @@
-
-import Markdown from 'react-markdown'
-
+import { useEffect, useState } from "react"
+import  ReactMarkdown  from "react-markdown";
 
 const blog = {
     content: `# Hello`
 }
 
 export default function Blog(){
+    const [value, setValue] = useState("")
+
+    useEffect(()=>{
+        async function getMD() {
+            const req = await fetch('');
+            const data = await req.json()
+            setValue(data.md);
+        }
+        
+        getMD();
+    },[])
+
     return <>
-    <div className=" text-white bg-black">
-        <Markdown className="prose lg:prose-xl text-white">{blog.content}</Markdown>
+    <div className="flex flex-row p-6 justify-around">
+        <div className="flex flex-col border-2">
+            <h1>Text box</h1>
+            <textarea name="example" id="" cols={70} rows={30} onChange={(e)=> setValue(e.target.value)}/>
+        </div>
+        <div className="flex flex-col">
+            <ReactMarkdown className="prose">{value}</ReactMarkdown>
+        </div>
     </div>
     </>
 }
